1ad0feafc524566eddbb1721dbd799a5efb63139
jucatupinamba <juniorbmx13@gmail.com>
2022-08-27 18:18:17 -0300

    feat: add classe e métodos produtos;

---
+++ src/main/java/br/com/suplementscommerce/controller/ProdutoController.java
@@ -0,0 +1,2 @@
+package br.com.suplementscommerce.controller;public class ProdutoController {
+}
---
+++ src/main/java/br/com/suplementscommerce/repository/ProdutoRepository.java
@@ -0,0 +1,2 @@
+package br.com.suplementscommerce.repository;public interface ProdutoRepository {
+}
