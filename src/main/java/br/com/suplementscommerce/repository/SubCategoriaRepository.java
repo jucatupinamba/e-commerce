aa07be28a12aebba0344f0b2b84b64781f91884e
jucatupinamba <juniorbmx13@gmail.com>
2022-09-15 14:39:04 -0300

    feat: add CRUD Subcategoria

---
+++ src/main/java/br/com/suplementscommerce/repository/SubCategoriaRepository.java
@@ -0,0 +1,8 @@
+package br.com.suplementscommerce.repository;
+
+import br.com.suplementscommerce.repository.entities.SubCategoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubCategoriaRepository extends JpaRepository<SubCategoria, Long> {
+
+}
