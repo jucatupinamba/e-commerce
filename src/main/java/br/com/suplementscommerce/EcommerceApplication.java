c962d69b267c1b7e020d9ea67068bb987a08247d
Juarez Martins <juniorbmx13@gmail.com>
2022-09-20 15:40:10 -0300

    refactor: aplicação View

--- src/main/java/br/com/suplementscommerce/EcommerceApplication.java
+++ src/main/java/br/com/suplementscommerce/EcommerceApplication.java
@@ -2,8 +2,10 @@ package br.com.suplementscommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.*")
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
