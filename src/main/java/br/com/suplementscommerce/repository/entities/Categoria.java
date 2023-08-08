6be54635ccad5aa12fe59444dfa6469676cb71f3
Juarez Martins <juniorbmx13@gmail.com>
2023-08-07 21:40:40 -0300

    "feat: Organização do código - teste postman"

--- src/main/java/br/com/suplementscommerce/repository/entities/Categoria.java
+++ src/main/java/br/com/suplementscommerce/repository/entities/Categoria.java
@@ -9,9 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Entity
 @Table(name = "categoria")
 public class Categoria {
@@ -19,6 +17,29 @@ public class Categoria {
     private Long id;
     @NotBlank
     private String nomeCategoria;
+
+    public Categoria(final Long id, final String nomeCategoria) {
+        this.id = id;
+        this.nomeCategoria = nomeCategoria;
+    }
+
+    public Categoria(){}
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getNomeCategoria() {
+        return this.nomeCategoria;
+    }
+
+    public void setNomeCategoria(final String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
 }
 
 
--- src/main/java/br/com/suplementscommerce/repository/entities/Cliente.java
+++ src/main/java/br/com/suplementscommerce/repository/entities/Cliente.java
@@ -1,16 +1,11 @@
 package br.com.suplementscommerce.repository.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Entity
 @Table(name = "clientes")
 public class Cliente {
@@ -27,4 +22,63 @@ public class Cliente {
     private String cpf;
     @NotBlank
     private String endereco;
+
+    public Cliente(final Long id, final String nome, final String idade, final String email, final String cpf, final String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    public Cliente(){}
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(final String nome) {
+        this.nome = nome;
+    }
+
+    public String getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(final String idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(final String cpf){
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return this.endereco;
+    }
+
+    public void setEndereco(final String endereco) {
+        this.endereco = endereco;
+    }
 }
