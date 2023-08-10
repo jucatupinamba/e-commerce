d16114fed05f209488903ad0609cd8579f75608f
Juarez Martins <juniorbmx13@gmail.com>
2023-08-10 19:34:43 -0300

    "feat: implementação Swagger Open API"

--- src/main/java/br/com/suplementscommerce/EcommerceApplication.java
+++ src/main/java/br/com/suplementscommerce/EcommerceApplication.java
@@ -1,11 +1,13 @@
 package br.com.suplementscommerce;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "br.com.*")
+@OpenAPIDefinition(info = @Info(title = "Swagger OpenApi", version = "1", description = "API desenvolvida para testes do OpenApi"))
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
