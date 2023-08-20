633bcf4d2634ad1612e36d77662b5570f2176775
Juarez Martins <juniorbmx13@gmail.com>
2023-08-20 12:24:51 -0300

    "feat: ENUM status do pedido; feat: desenvolvendo classe pedido; feat criação da classe item pedido"

--- src/main/java/br/com/suplementscommerce/repository/entities/Pedido.java
+++ src/main/java/br/com/suplementscommerce/repository/entities/Pedido.java
@@ -1,10 +1,12 @@
 package br.com.suplementscommerce.repository.entities;
 
+import br.com.suplementscommerce.repository.entities.enums.StatusPedido;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import javax.persistence.*;
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -26,4 +28,74 @@ public class Pedido {
     private Set<ItemPedido> items = new HashSet<>();
     @OneToOne(mappedBy = "pedido", cascade = CascadeType.ALL)
     private Pagamento pagamento;
+
+    public Pedido(){}
+
+    public Pedido(Long id, Instant momento, StatusPedido statusPedido, Cliente cliente) {
+        this.id = id;
+        this.momento = momento;
+        setStatusPedido(statusPedido);
+        this.cliente = cliente;
+        this.items = items;
+        this.pagamento = pagamento;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public Instant getMomento() {
+        return this.momento;
+    }
+
+    public void setMomento(final Instant momento) {
+        this.momento = momento;
+    }
+
+    public StatusPedido getStatusPedido() {
+        return StatusPedido.valueOf(statusPedido);
+    }
+
+    public void setStatusPedido(StatusPedido statusPedido) {
+        if(statusPedido!= null) {
+            this.statusPedido = statusPedido.getCodigo();
+        }
+    }
+
+    public Cliente getCliente() {
+        return this.cliente;
+    }
+
+    public void setCliente(final Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Set<ItemPedido> getItems() {
+        return this.items;
+    }
+
+    public Pagamento getPagamento() {
+        return this.pagamento;
+    }
+
+    public void setPagamento(final Pagamento pagamento) {
+        this.pagamento = pagamento;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (null == o || this.getClass() != o.getClass()) return false;
+        final Pedido pedido = (Pedido) o;
+        return Objects.equals(this.id, pedido.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id);
+    }
 }
